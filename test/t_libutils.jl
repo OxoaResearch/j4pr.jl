b3c064026e6d549ab261a896bba33eb428e9124a
Corneliu Cofaru <cornel@oxoaresearch.com>
2017-10-26 16:21:36 +0200

    Fixed libutils.jl tests

--- test/t_libutils.jl
+++ test/t_libutils.jl
@@ -6,19 +6,19 @@ function t_libutils()
 	references = ["a","a","c","c","b","b"];
 	predictions = ["a","b","c","c","a","a"];
 
-	C = confusionmatrix(predictions,references)
+	C = j4pr.confusionmatrix(predictions,references)
 	Cref = [1.0  2.0  0.0; 1.0  0.0  0.0; 0.0  0.0  2.0]
 	Base.Test.@test C == Cref
 	
-	Cn = confusionmatrix(predictions,references;normalize=true)
+	Cn = j4pr.confusionmatrix(predictions,references;normalize=true)
 	Cref = [0.5  1.0  0.0; 0.5  0.0  0.0; 0.0  0.0  1.0]
 	Base.Test.@test Cn == Cref
 	
-	Cp = confusionmatrix(predictions,references;positive="a")
+	Cp = j4pr.confusionmatrix(predictions,references;positive="a")
 	Cref = [1.0 2.0; 1.0  2.0]
 	Base.Test.@test Cp == Cref
 
-	Cpn = confusionmatrix(predictions,references;normalize=true,positive="a")
+	Cpn = j4pr.confusionmatrix(predictions,references;normalize=true,positive="a")
 	Cref = [0.5  0.5; 0.5  0.5]
 	Base.Test.@test Cpn == Cref
 
